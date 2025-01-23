569902146bdd13b46df39a337868e4ee0ac4750d
muyiwa <muyiwa100@yahoo.com>
2025-01-23 08:06:34 +0000

    change index for instances

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-import * as pulumi from "@pulumi/pulumi";
+import { masterInstance } from "./instances/master"
+import { workerAsg } from "./instances/worker";
+import { bastionHost } from "./instances/bastion";
+
+
+
+async function main() {
+    const master = await masterInstance("Master");
+    const worker = workerAsg
+    const bastion = bastionHost
+
+   
+    // Return all instances for use in other parts of your Pulumi program
+    return { master, worker, bastion };
+}
+
+export const instances = main();
